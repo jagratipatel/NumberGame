69987110eb54b194051843c12bda024dfadbe666
Jagrati <pateljagrati18@gmail.com>
2020-01-24 00:49:59 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,8 @@ class Card extends React.Component{
         return( 
                 <div id="cards">
                     {this.props.isCardVisible ? (<div className="card">{this.props.value1}</div>) :(<div className="card" onClick={this.props.setVisibility(1)}></div>)}
-                    {this.props.isCardVisible ? (<div className="card">{this.props.value2}</div>) :(<div className="card" onClick={this.props.setVisibility(1)}></div>)}
-                    {this.props.isCardVisible ? (<div className="card">{this.props.value3}</div>) :(<div className="card" onClick={this.props.setVisibility(1)}></div>)}
+                    {this.props.isCardVisible ? (<div className="card">{this.props.value2}</div>) :(<div className="card" onClick={this.props.setVisibility(2)}></div>)}
+                    {this.props.isCardVisible ? (<div className="card">{this.props.value3}</div>) :(<div className="card" onClick={this.props.setVisibility(3)}></div>)}
                 </div>
             )
 
